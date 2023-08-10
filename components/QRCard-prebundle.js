333533bcb7caba83dbe22b64d682607d10f6cf97
Jason Sherrick <jasonsherrick@gmail.com>
2023-08-09 22:09:32 -0500

    Complete componente

---
+++ components/QRCard-prebundle.js
@@ -0,0 +1,125 @@
+const QRCode = require('qrcode');
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        :host {
+            --light-gray: hsl(212, 45%, 89%);
+            --grayish-blue: hsl(220, 15%, 55%);
+            --dark-blue: hsl(218, 44%, 22%);
+        }
+
+        .card, .description {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        ::slotted(h2), ::slotted(p) {
+            margin: 0;
+            padding: 0;
+        }
+
+        ::slotted(h2) {
+            font-size: 1.5rem;
+        }
+
+        ::slotted(p) {
+            margin-bottom: 2rem;
+            color: var(--grayish-blue);
+        }
+
+        .card {
+            gap: 1.5rem;
+            box-sizing: border-box;
+            font-family: 'Outfit', system-ui, sans-serif;
+            border-radius: 1rem;
+            background-color: white;
+            padding: 1rem;
+            max-width: calc(290px + 2rem);
+        }
+
+        .qr-code {
+            background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><rect x="0" y="0" width="100" height="100" fill="%232b7dfa"></rect><circle cx="0" cy="0" r="60" fill="%233685fe"></circle><circle cx="95" cy="125" r="55" fill="%233685fe"></circle></svg>');
+            width: 290px;
+            height: 290px;
+            border-radius: 0.75rem;
+        }
+
+        canvas {}
+        
+        .description {
+            gap: 0.75rem;
+            color: var(--dark-blue);
+            text-align: center;
+            padding: 0 0.5rem;
+        }
+    </style>
+
+    <div class="card">
+        <div class="qr-code">
+            <canvas></canvas>
+        </div>
+        <div class="description">
+            <slot name="heading"></slot>
+            <slot name="caption"></slot>
+        </div>
+    </div>
+`;
+
+class QRCard extends HTMLElement
+{
+    constructor()
+    {
+        super();
+
+        const shadow = this.attachShadow({ mode: 'open'});
+        // Append the content
+        shadow.append(template.content.cloneNode(true));
+
+        this.canvas = shadow.querySelector('canvas');
+    }
+
+    static get observedAttributes()
+    {
+        return['url'];
+    }
+
+    connectedCallback()
+    {
+        const currentUrl = this.url;
+
+        if(currentUrl) {
+            this.paintQrCode(currentUrl);
+        }
+    }
+
+    get url()
+    {
+        return this.getAttribute('url');
+    }
+
+    set url(resource)
+    {
+        this.setAttribute('url', resource);
+    }
+
+    paintQrCode(str)
+    {
+        QRCode.toCanvas(
+            this.canvas,
+            str,
+            {
+                errorCorrectionLevel: "M",  
+                color: {
+                    dark: '#fff',
+                    light: '#0000'
+                }
+            },
+            (err) => {
+                if (err) console.log(err);
+            }
+        );
+    }
+}
+
+customElements.define("qr-card", QRCard);
--- index.js
+++ index.js
@@ -3,13 +3,15 @@
  */
 
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = 3000;
+app.use(express.static('./'));
 
 app.get('/', (req, res) => {
-    res.sendFile('./index.html');
+    res.sendFile('index.html');
 });
 
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log('listening on port 3000');
 })
